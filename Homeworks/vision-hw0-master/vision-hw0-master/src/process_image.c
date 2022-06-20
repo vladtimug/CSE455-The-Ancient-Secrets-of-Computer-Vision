d93929cdf8a182790105def9ec51390269b23b4a
vlad <timuvlad@yahoo.com>
2022-06-20 08:41:39 +0300

    Defined get_pixel and set_pixel functions

--- Homeworks/vision-hw0-master/vision-hw0-master/src/process_image.c
+++ Homeworks/vision-hw0-master/vision-hw0-master/src/process_image.c
@@ -4,15 +4,56 @@
 #include <math.h>
 #include "image.h"
 
+/**
+ * @brief Get pixel value located at column x, row y and channel c
+ * 
+ * @param im Image object
+ * @param x Image column index
+ * @param y Image row index
+ * @param c Image channel index
+ * @return float 
+ */
 float get_pixel(image im, int x, int y, int c)
 {
-    // TODO Fill this in
-    return 0;
+    // Use clamping pad strategy to avoid indexing image data at invalid values
+    if(x < 0) {
+        x = 0;
+    } else if(x > im.w) {
+        x = im.w - 1;
+    }
+
+    if(y < 0) {
+        y = 0;
+    } else if(y > im.h) {
+        y = im.h - 1;
+    }
+
+    if(c < 0) {
+        c = 0;
+    } else if(c > im.c) {
+        c = im.c - 1;
+    }
+
+    int pixel_position = x + y * im.w + c*im.w*im.h;
+    
+    return im.data[pixel_position];
 }
 
+/**
+ * @brief Set the pixel value located at column x, row y and channel c to value v
+ * 
+ * @param im Image object
+ * @param x Image column index
+ * @param y Image row index
+ * @param c Image channel index
+ * @param v New pixel value
+ */
 void set_pixel(image im, int x, int y, int c, float v)
 {
-    // TODO Fill this in
+    if ((x >= 0 && x < im.w) && (y >=0 && y < im.h) && (c >= 0 && c < im.c)) {
+        int pixel_position = x + y * im.w + c*im.w*im.h;
+        im.data[pixel_position] = v;
+    }
 }
 
 image copy_image(image im)
